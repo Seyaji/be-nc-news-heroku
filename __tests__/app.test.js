546d9736898246f746500d5a14fcfc35eaf7547c
Alex <alex@Alexs-MBP.lan>
2022-02-17 14:57:56 +0000

    completes getArticlesByID and the relevant tests, also did a minor refactor

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,7 +12,7 @@ afterAll(() => {
 
 describe('API tests', () => {
    describe('/api/topics tests', () => {
-      describe('1. GET /api/topics', () => {
+      describe('1. TOPICS GET /api/topics', () => {
          test('status 200: responds with an array of topic objects each containing the properties slug and description', () => {
             return request(app)
             .get('/api/topics')
@@ -31,7 +31,7 @@ describe('API tests', () => {
             })
          })
       }) // end of 1. GET /api/topics test
-      describe('2. POST /api/topics', () => {
+      describe('2. TOPICS POST /api/topics', () => {
          test('status 201: responds with newly added topic to the database and checks to see if the topics have increased', () => {
             const newTopic = { slug: 'coding', description: 'Javascript 101!' }
             return request(app)
@@ -52,39 +52,62 @@ describe('API tests', () => {
             })
          })
       })
+      describe('1. ARTICLES GET /api/articles/:id should return the article associated with the id', () => {
+         test('status 200: returns the article when given an id', () => {
+            return request(app)
+            .get('/api/articles/1')
+            .expect(200)
+            .then((result) => {
+               expect(result.body.article_id).toBe(1)
+               expect(result.body).toEqual(
+                  expect.objectContaining({
+                     article_id: expect.any(Number),
+                     title: expect.any(String),
+                     topic: expect.any(String),
+                     author: expect.any(String),
+                     body: expect.any(String),
+                     created_at: expect.any(String),
+                     votes: expect.any(Number),
+                  })
+               );
+            })
+         })
+      })
    }) // end of api topics tests
    describe('global error tests', () => {
       // Utility Test Functions
       const badPath = (path) => path.replace(/[a-z]$/i, 'badPath')
       
-      describe('status 404: tests GET API paths for 400 error handling', () => {
+      describe('status 404: tests for non existent paths', () => {
          const APIPaths = [
-            [badPath('/api/topics'), `path not found...`]
+            [badPath('/api/topics'), `path not found...`],
+            ['/api/not-a-path', `path not found...`],
          ]
-         test.concurrent.each(APIPaths)('.get(%s)', (path, expected) => {
+         test.concurrent.each(APIPaths)
+         ('.get(%s)', (path, expected) => {
             return request(app)
             .get(path)
             .expect(404)
-            .then(({ body }) => {
-               console.log(body)
-               expect(body.message).toBe(expected)
+            .then(({ body: {message} }) => {
+               expect(message).toBe(expected)
             })
          })
       })
-      // describe('status 400: tests bad request paths for API', () => {
-      //    const badRequest = (request) => request.replace(/[a-z]$/i, 'badRequest')
-      //    const requestPaths = [
-      //       ['placeholderInput', 'placeholderExpected']
-      //    ]
-      //    test.concurrent.each(requestPaths)('.get(%s)', async (path, expected) => {
-      //       return request(app)
-      //       .get(path)
-      //       .expect(400)
-      //       .then(({ body: { message } }) => {
-      //          expect(message).toBe(expected)
-      //       })
-      //    })
-      // })
+      describe('status 404: tests parametric endpoints that dont exist', () => {
+         const parametricEndpoints = [
+            ['/api/articles/98765', 'Article 98765 either does not exist or cannot be found'],
+            ['/api/articles/BadID-Format', 'id BadID-Format is not in a valid format, it should be a number']
+         ]
+         test.concurrent.each(parametricEndpoints)
+         ('.get(%s)', (path, expected) => {
+            return request(app)
+            .get(path)
+            .expect(404)
+            .then(({ body: { message } }) => {
+               expect(message).toBe(expected)
+            })
+         })
+      })
       // describe('status 500: internat server error', async () => {
       //    test ('500: server error test', () => {
       //       const server500Mock = jest.fn().mockRejectedValue(new Error('internal server error...'))
--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,11 +1,17 @@
-const {selectTopics, insertTopic} = require('../models/topics.model')
+const {selectTopics, selectArticleByID, insertTopic,} = require('../models/topics.model')
 
 exports.getTopics = (req, res, next) => {
    selectTopics().then((result) => res.status(200).send(result))
    .catch((err) => next(err));
 }
 
+exports.getArticleByID = (req, res, next) => {
+   const {id} = req.params
+   selectArticleByID(id).then((result) => res.status(200).send(result))
+   .catch((err) => next(err))
+}
+
 exports.postTopic = (req, res, next) => {
-   insertTopic(req.body).then((topics) => res.status(201).send( { topics } ))
+   insertTopic(req.body).then((topics) => res.status(201).send({ topics }))
    .catch((err) => next(err));
 }
